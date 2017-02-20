d7e579f5f29cfd2f636b2711a045b2379c3a8c67
PHPfamo <muris.mulahalilovic@gmail.com>
2017-02-20 20:43:43 +0100

    4 file initial

---
+++ 4_basic_object_creation.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+class Car {
+
+	/*
+	This is a fun part ;) now remeber everything is an object, 
+	as we are thinking object oriented way we need to access things inside our class,
+	best way to do it is by creating our first object wit a new key word.
+	One class can have more then one object, so this is a good time to think of our class
+	as a blueprint for large scale copy/paste operation, but without copying pasting all the time.
+	This is a thing we call instance (creating object out of a class).
+
+
+		*/
+
+
+
+}
+
+
+$bmw= new Car(); //Proper way of creating our first instance (first object)
+
+
+
+
+ ?>
