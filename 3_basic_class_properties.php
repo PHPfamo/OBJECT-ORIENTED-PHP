3468bc743ceb230f25646d436d254010e030dc15
PHPfamo <muris.mulahalilovic@gmail.com>
2017-02-20 20:27:52 +0100

    3 file initial

---
+++ 3_basic_class_properties.php
@@ -0,0 +1,26 @@
+<?php 
+
+
+class Car {
+
+	public $type='cabrio';
+	public $color='black';
+	public $manufacturer='bmw';
+	public $numberOfDoors=4;
+
+
+	/*
+	As you can see, just like in procedural way you can place values for 
+	our properties, just like that
+
+	*/
+
+
+}
+
+
+
+
+
+
+ ?>
