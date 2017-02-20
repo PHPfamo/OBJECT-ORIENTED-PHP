8ac5e9605eaff3366ede993a1cffd23db64be318
PHPfamo <muris.mulahalilovic@gmail.com>
2017-02-20 20:23:47 +0100

    2 file inital

---
+++ 2_basic_class_properties.php
@@ -0,0 +1,30 @@
+<?php 
+
+
+class Car {
+
+	public $type;
+	public $color;
+	public $manufacturer;
+
+
+	/*At this time we only place some properties for our class using reserved key "public"
+	you will often use this keyword, but if you forget to place it before any properties the 
+	default valu will be also know as "public".
+
+	Naming convention is to use lowercasing first letter for naming properties,
+	use CamelCase naming convention if the name has more then one word
+
+	for example $myPropertyName or $carOtherPropertie
+
+	*/
+
+
+}
+
+
+
+
+
+
+ ?>
