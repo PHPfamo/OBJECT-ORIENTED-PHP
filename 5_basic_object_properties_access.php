b10cbe09a70ba67a04d3b9fff51b9229341b3b32
PHPfamo <muris.mulahalilovic@gmail.com>
2017-02-20 20:58:02 +0100

    5 file initial

---
+++ 5_basic_object_properties_access.php
@@ -0,0 +1,30 @@
+<?php 
+
+
+class Car {
+
+
+	public $type='sport car',
+	$name='Muris';
+
+
+
+
+
+}
+
+
+$bmw= new Car(); 
+echo "Hello ".$bmw->name." we see you like bmw as a ".$bmw->type;
+
+/*First we placed some basic properties and then we 
+made our object (instance). Once we created instance we can acces properties and it's values
+by usin -> operator (minus, greater then). Echoing is super easy as you can see.
+  
+
+*/
+
+
+
+
+ ?>
